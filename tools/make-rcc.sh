76314e645f5b22f42a9b16050f24eb3c8e2a4bed
Orange233_Over_DESKTOP-KLR9OIP <orange_233@foxmail.com>
2026-09-15 02:44:33 +0800

    Fixed with DeepSeek V4 Flash.

--- tools/make-rcc.sh
+++ tools/make-rcc.sh
@@ -2,4 +2,4 @@ dir=$(dirname "$0")
 cd "$dir" || exit
 cd ../src/count_anywhere || exit
 
-pyside6-rcc resources.qrc -o rc_resources.py
+pyside6-rcc resources.qrc -o rc_resources.py --compress-algo zlib
